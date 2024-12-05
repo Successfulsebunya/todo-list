d8cbb57bcb958cbfbd38735b626fdbca0dbd0f74
MoCursor <ssebunya.moses2@gmail.com>
2024-12-05 10:01:59 +0300

    Better styling and Improved visual

--- index.php
+++ index.php
@@ -1,24 +1,29 @@
+<?php
+require 'config.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Todo List</title>
-  <link rel="stylesheet" 
-          type="text/css" href="style.css" />
+  <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 
 <body>
-  <nav>
-    <a class="heading" href="#">ToDo App</a>
-  </nav>
+
   <div class="container">
+
+  <nav>
+    <a class="heading" href="#">To Do List</a>
+  </nav>
     <div class="input-area">
       <form method="POST" action="add_task.php">
-        <input type="text" name="task" 
-                 placeholder="write your tasks here..." required />
+        <input type="text" name="task" placeholder="Write your tasks here..." required />
         <button class="btn" name="add">
-              Add Task
-          </button>
+          Add Task
+        </button>
       </form>
     </div>
     <table class="table">
@@ -32,42 +37,37 @@
       </thead>
       <tbody>
         <?php
-                require 'config.php';
-                $fetchingtasks = 
-mysqli_query($db, "SELECT * FROM `task` ORDER BY `task_id` ASC")
-or die(mysqli_error($db));
-                $count = 1;
-                while ($fetch = $fetchingtasks->fetch_array()) {
-                    ?>
-        <tr class="border-bottom">
-          <td>
-            <?php echo $count++ ?>
-          </td>
-          <td>
-            <?php echo $fetch['task'] ?>
-          </td>
-          <td>
-            <?php echo $fetch['status'] ?>
-          </td>
-          <td colspan="2" class="action">
-            <?php
-                if ($fetch['status'] != "Done")
-                {
-                    echo
-'<a href="update_task.php?task_id=' . $fetch['task_id'] . '" 
-class="btn-completed"></a>';
-                }
-            ?>
-            <a href=
-"delete_task.php?task_id=<?php echo $fetch['task_id'] ?>" 
-               class="btn-remove">
-              
-            </a>
-          </td>
-        </tr>
+        $fetchingtasks = mysqli_query($db, "SELECT * FROM `task` ORDER BY `task_id` ASC") or die(mysqli_error($db));
+        $count = 1;
+        while ($fetch = $fetchingtasks->fetch_array()) {
+        ?>
+          <tr class="border-bottom">
+            <td>
+              <?php echo $count++; ?>
+            </td>
+            <td>
+              <?php echo htmlspecialchars($fetch['task']); ?>
+            </td>
+            <td>
+              <?php echo htmlspecialchars($fetch['status']); ?>
+            </td>
+            <td colspan="2" class="action">
+              <?php if ($fetch['status'] != "Done") { ?>
+                <a href="update_task.php?task_id=<?= $fetch['task_id'] ?>" class="btn-completed">
+                  Done
+                </a>
+                <a href="delete_task.php?task_id=<?= $fetch['task_id'] ?>" class="btn-remove">
+                  Delete
+                </a>
+              <?php } else { ?>
+                <a class="btn-completed disabled">Done</a>
+                <a class="btn-remove disabled">Delete</a>
+              <?php } ?>
+            </td>
+          </tr>
         <?php
-                }
-            ?>
+        }
+        ?>
       </tbody>
     </table>
   </div>
